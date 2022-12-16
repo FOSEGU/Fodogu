bca77f0190b2151fc603095fb94b1e993ab1572b
M4ndU <33446356+M4ndU@users.noreply.github.com>
2022-12-16 00:15:15 -0500

    fix

--- modulesV3/GPSPayload.py
+++ modulesV3/GPSPayload.py
@@ -30,15 +30,15 @@ class GPSPayloadV3:
 
         data['time'] = struct.unpack('I', payload[4:8])[0]
 
-        longitude = struct.unpack('f', payload[8:12])[0]     # parse double (float) longitude
-        data['longitude'] = self.convertpos(longitude)      # convert radians to degrees
+        data['longitude'] = struct.unpack('f', payload[8:12])[0]  / 10**7   # parse double (float) longitude
+        #data['longitude'] = self.convertpos(longitude)      # convert radians to degrees
 
-        latitude = struct.unpack('f', payload[12:16])[0]     # parse double (float) latitude
-        data['latitude'] = self.convertpos(latitude)        # convert radians to degrees
+        data['latitude'] = struct.unpack('f', payload[12:16])[0]  / 10**7   # parse double (float) latitude
+        #data['latitude'] = self.convertpos(latitude)        # convert radians to degrees
 
-        data['velN'] = struct.unpack('f', payload[20:24])[0]
-        data['velE'] = struct.unpack('f', payload[24:28])[0]
-        data['velD'] = struct.unpack('f', payload[28:32])[0]
+        data['velN'] = struct.unpack('f', payload[20:24])[0] / 100
+        data['velE'] = struct.unpack('f', payload[24:28])[0] / 100
+        data['velD'] = struct.unpack('f', payload[28:32])[0] / 100
         data['vel'] = float(math.sqrt(math.pow(data['velN'],2) + math.pow(data['velE'],2) + math.pow(data['velD'],2)))
 
         # only output legitimate location data
